6ec97aa04476eefcc850ab703e6e05976d6828d5
JefteCosta <jefteamorim@gmail.com>
2025-01-10 16:05:57 -0300

    fix(taskdefinition): variaveis de ambiente para host

--- start/env.ts
+++ start/env.ts
@@ -18,7 +18,7 @@ export default await Env.create(new URL('../', import.meta.url), {
   HOST: Env.schema.string({ format: 'host' }),
   LOG_LEVEL: Env.schema.enum(['fatal', 'error', 'warn', 'info', 'debug', 'trace']),
 
-  REDIS_HOST: Env.schema.string({ format: 'host' }),
-  REDIS_PORT: Env.schema.number(),
-  REDIS_PASSWORD: Env.schema.string.optional()
+  // REDIS_HOST: Env.schema.string({ format: 'host' }),
+  // REDIS_PORT: Env.schema.number(),
+  // REDIS_PASSWORD: Env.schema.string.optional()
 })
