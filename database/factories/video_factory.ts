cdb0160968ea521a5e9f9b3469e761b210387229
JefteCosta <jefteamorim@gmail.com>
2025-01-10 15:01:37 -0300

    fix(video_factory): ateração na factory para rodar o build

--- database/factories/video_factory.ts
+++ database/factories/video_factory.ts
@@ -3,6 +3,7 @@ import Video from '#models/video'
 
 export const VideoFactory = factory
   .define(Video, async ({ faker }) => {
+    console.log('o que tem aqui!', faker)
     return {}
   })
   .build()
